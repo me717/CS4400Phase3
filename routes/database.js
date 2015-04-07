2efeb0357d34f1b525764b349bba009d9d585a46
unknown <amorlan3@gatech.edu>
2015-04-07 15:13:09 -0400

    More login info stuff

--- routes/database.js
+++ routes/database.js
@@ -5,9 +5,8 @@ var format = require('string-template');
 var credentials = require('./credentials');
 var router = express.Router();
 
-/* LOGIN. */
-router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Database' });
+//Login
+router.get('/login', function(req, res, next) {
     var query = "SELECT username FROM User WHERE '{username}' = username";
     query = query + " AND '{password}' = password";
     query = format(query ,{
@@ -15,15 +14,46 @@ router.get('/', function(req, res, next) {
                         password: req.query.password
     });
     executeQuery(query, function(error, results, fields){
-        if(results.length){
+        if(results.length) {
             res.status(200);
             res.send(results);
-        } else{
+        } else {
             res.render('index', { title: "Empty"});
         }
     });
 });
 
+//Register
+router.post('/register', function(req, res, next)) {
+    var query = "INSERT INTO User(username, password)"
+    query = query + "VALUES ('{username}', '{password}')";
+    query = format(query, {
+        username: req.query.username,
+        password: req.query.password
+    });
+    executeQuery(query, function(error, results, fields) {
+        if(!error) {
+            res.status(200);
+            res.send(results);
+        } else {
+            //TODO error handling
+            res.status(500);
+        }
+    });
+}
+
+//Track location
+router.get('/trackLocation', function(req, res, next)) {
+    var query = "SELECT Book.shelfNumber, Shelf.aisleNumber, " +
+                "Floor.floorNumber, Book.subject " + 
+                "FROM Book " +
+                "JOIN Shelf ON Book.shelfNumber = Shelf.shelfNumber " +
+                "JOIN Floor ON Shelf.floorNumber = Floor.floorNumber" +
+                "WHERE Book.isbn = {isbn}";
+    query = format(query, {
+        isbn: req.query.isbn
+    });
+}
 
 function executeQuery(query, callback) {
     var connection = mysql.createConnection(credentials);
