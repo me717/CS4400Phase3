c08a4116fdadfc34e11bbae95a134a99bf32a229
Stephen Song <stephen.song@gatech.edu>
2015-04-19 19:11:56 -0400

    Completed basic functionality for the registration page.

--- public/scripts/actions.js
+++ public/scripts/actions.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+	// login page
 	$('#login-btn').click(function() {
 		$.ajax({
 			url: "db/login",
@@ -12,7 +14,38 @@ $(document).ready(function(){
 			error: function(xhr, status, error) {
 				alert("error");
 				console.log(error.code);
+				window.location.href = "";
 			}
 		});
 	});
+	$('#registration-btn').click(function() {
+		window.location.href = "registration";
+	});
+
+	// registration page
+	$('#create-account-btn').click(function() {
+		var username = $('#registration-username').val();
+		var password = $('#registration-password').val();
+		var confirm_password = $('#registration-confirm-password').val();
+		if (username === '' || password === '' || confirm_password === '') {
+			$("#registration-form").addClass("error");
+			$("#registration-error-header").text("Incomplete Form");
+			$("#registration-error-body").text("One or more fields have been left blank.");
+		} else if (false) { // see if there's a duplicate user
+			$("#registration-form").addClass("error");
+			$("#registration-error-header").text("Duplicate User");
+			$("#registration-error-body").text("Another user with this username already exists.");
+		} else if (password != confirm_password) {
+			$("#registration-form").addClass("error");
+			$("#registration-error-header").text("Passwords do not match");
+			$("#registration-error-body").text("Double check if you typed in your passwords correctly.");
+		} else {
+			window.location.href = "profile";
+		}
+	});
+
+	$('select.dropdown').dropdown();
+
+
+
 });
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Login' });
+});
+
+/* GET registration. */
+router.get('/registration', function(req, res, next) {
+  res.render('registration', { title: 'New User Registration' });
+});
+
+/* GET registration. */
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { title: 'Create Profile' });
 });
 
 module.exports = router;
